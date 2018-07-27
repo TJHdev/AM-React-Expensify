f06580dd1c9d0f4e353f0b03aa9aa7c30ad4659e
Thomas Hanna <thomasjhanna@gmail.com>
2018-07-27 15:51:34 +0100

    Added object and array spread operator example

--- src/playground/3-redux-expensify.js
+++ src/playground/3-redux-expensify.js
@@ -1,9 +1,48 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
 
 // ADD_EXPENSE
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description: description,
+    note: note,
+    amount: amount,
+    createdAt: createdAt
+  }
+})
+
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id: id
+}) 
+
 // EDIT_EXPENSE
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id: id,
+  updates
+})
+
 // SET_TEXT_FILTER
+// store.dispatch(setTextFilter('rent'));
+
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text: text
+})
+
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -15,6 +54,23 @@ const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => { 
+        return id !== action.id; 
+      });
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense;
+        }
+      });
     default: 
       return state;
   }
@@ -30,6 +86,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      }
     default: 
       return state;
   }
@@ -44,7 +105,30 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+const expenseOne = store.dispatch(addExpense({
+  description: 'This is the first expense',
+  amount: 500
+}));
+const expenseTwo = store.dispatch(addExpense({
+  description: 'Coffee',
+  amount: 300
+}));
+const expenseThree = store.dispatch(addExpense({
+  description: 'Car Insurance',
+  amount: 2000
+}));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 555 }));
+
+store.dispatch(setTextFilter('rent'));
+
+store.dispatch(setTextFilter(''));
 
 const demoState = {
   expenses: [{
@@ -61,3 +145,14 @@ const demoState = {
     endDate: null
   }
 };
+
+const user = {
+  name: 'Jen',
+  age: 24
+};
+
+console.log({
+  ...user,
+  location: 'York',
+  age: 30
+})
