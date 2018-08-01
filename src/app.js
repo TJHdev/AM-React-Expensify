a3f53310b4579f4c081365dd14471d5738901a99
Thomas Hanna <thomasjhanna@gmail.com>
2018-08-01 15:33:49 +0100

    Changes for production webpack and css loader

--- src/app.js
+++ src/app.js
@@ -15,22 +15,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-
-
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-})
-
-console.log(store.getState());
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 9999, createdAt: 1532986973481 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 4, createdAt: 1533898800000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 1000, createdAt: 1531911600000 }));
-// store.dispatch(setTextFilter('gas'));
-
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
