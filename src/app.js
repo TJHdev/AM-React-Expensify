4e039146def46559106e2b95965713d0a7b307b7
Thomas Hanna <thomasjhanna@gmail.com>
2018-08-03 13:10:53 +0100

    Added login and logout tests actions and reducers

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import getVisibleExpenses from './selectors/expenses';
 import configureStore from './store/configureStore';
-
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-
 import { firebase } from './firebase/firebase';
+import { login, logout } from './actions/auth';
+
 
 const store = configureStore();
 
@@ -22,16 +20,28 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+let hasRendered = false;
+const renderApp = () => {
+  if(!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
-});
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    console.log('logged in')
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
   } else {
-    console.log('logged out')
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
   }
 });
